a9a373cb928ba0996c5860b9d894d1e46e6db8f0
unknown <laibajmi227@gmail.com>
2021-03-11 17:08:12 +0530

    COMMIT

--- src/App.js
+++ src/App.js
@@ -1,39 +1,46 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import SearchBox from './SearchBox';
 import './App.css'
- import CardList from './CardList';
- import {robots} from './robot';
+import CardList from './CardList';
+import Scroll from './Scroll';
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super()
-    this.state={
-      robots:[],
-      searchfeild:'',
+    this.state = {
+      robots: [],
+      searchfeild: '',
 
     }
   }
-  onSearchchange=(event)=>{
-   this.setState({searchfeild:event.target.value})
- 
- 
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ robots: users }))
   }
-  render(){ 
-    const filteredRobots=this.state.robots.filter(robots=>{
-      return(robots.name.toLowerCase().includes(this.state.searchfeild.toLowerCase())
-       
-    )})
-  return (
-    <div className='tc'>
-    <h1>RoboFriends</h1>
-    <SearchBox searchChange={this.onSearchchange}/>
-    <div>
-      <CardList robots={filteredRobots}/>,
-    
-      </div>
-      </div>   
- )
+  onSearchchange = (event) => {
+    this.setState({ searchfeild: event.target.value })
   }
-  
+  render() {
+    const filteredRobots = this.state.robots.filter(robot => {
+      return robot.name.toLowerCase().includes(this.state.searchfeild.toLowerCase())
+    })
+    if (this.state.robots.length === 0) {
+      return <h1>Loading</h1>
+    } else {
+      return (
+        <div className='tc'>
+          <h1>RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchchange} />
+          <div>
+            <Scroll>
+              <CardList robots={filteredRobots} />,
+            </Scroll>
+          </div>
+       </div>
+      )
+    }
+  }
+
 }
- export default App;
+export default App;
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,15 +1,30 @@
 import React from 'react';
 
-const SearchBox = ({searchfield,searchChange}) => {
+const SearchBox = ({ searchChange }) => {
+    
     return (
         <div className='pa2'>
             <input
-            className=' pa3 ba b--green bg-lightest-blue'
-             type='Search'
-             placeholder='search robos'
-             onChange={searchChange} />
-             
-   </div>
+                className=' pa3 ba b--green bg-lightest-blue'
+                type='Search'
+                placeholder='search robos'
+                onChange={searchChange} />
+        </div>
     );
 }
 export default SearchBox;
+
+// const a = {
+//     name: "Laiba",
+//     age: 20
+// }
+
+// console.log(a.name);
+// console.log(a.age);
+
+// const { name, age } = a;
+
+// const name = a.name;
+// const age = a.age;
+
+// const {searchChange} = props;
